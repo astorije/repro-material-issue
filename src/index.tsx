c55cee678c9ca2f7a205e6a0679886b678f97c2a
Jérémie Astori <jeremie@astori.fr>
2019-09-13 13:45:42 -0400

    "Fix" the inject order issue by updating dependencies
    Note that this is not something that was fixed in the latest version.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Button } from '@material-ui/core';
 import { StylesProvider } from '@material-ui/styles';
 
-// Thanks to injectFirst, this button should have a red background but does not
+// As long as there is no newer @material-ui/styles release, this button will be red
 export const App = () => (
   <StylesProvider injectFirst>
     <Button variant="contained">Hello</Button>
